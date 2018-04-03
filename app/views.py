08d40c38a41897f1f50f7a318ffd39307c10a375
Nguyen Dan <nguyendan@hotmail.com.vn>
2018-04-03 23:31:46 +0700

    SignIn and SignUp

--- app/views.py
+++ app/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+import logging
+import pyrebase
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
-# Create your views here.
+config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+authen = firebase.auth()
+database = firebase.database()
+
+
+def index(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    auth.logout(request)
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        try:
+            user = authen.sign_in_with_email_and_password(email, password)
+            if user:
+                request.session['iud'] = str(user['idToken'])
+            return redirect('home')
+        except Exception:
+            message = 'invalid credentials'
+            messages.warning(request, message)
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'accounts/login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        try:
+            user = authen.create_user_with_email_and_password(email, password)
+            if user:
+                uid = user['localId']
+                data = {'name': name, 'status': '1'}
+                database.child('users').child(uid).child('details').set(data)
+                return redirect('login')
+        except Exception as e:
+            messages.warning(request, str(e))
+    return render(request, 'accounts/signup.html')
--- djrebase/settings.py
+++ djrebase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
